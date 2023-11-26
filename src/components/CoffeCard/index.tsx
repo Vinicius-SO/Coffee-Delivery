366a7c3a293adc5bb4af35316164b89eaa7d761b
unknown <vhisoares39@gmail.com>
2023-11-26 00:45:29 -0300

    Add: Scroll reveal

--- src/components/CoffeCard/index.tsx
+++ src/components/CoffeCard/index.tsx
@@ -2,8 +2,9 @@ import { BuyContainer, CoffeeCardContainer } from "./styles";
 import Coffee from '../../assets/Coffee.svg'
 import { Minus, Plus, ShoppingCart } from "phosphor-react";
 import { ShoppingCartContext } from '../../context/ShoppingCartContext'
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import ScrollReveal from 'scrollreveal'
 
 
 interface CoffeeItem{
@@ -23,6 +24,7 @@ interface CoffeeCardProps{
 export function CoffeeCard ({coffe}:CoffeeCardProps){
     const {cart, addProductToShopCart} = useContext(ShoppingCartContext)
 
+    
 
     const [count, setCount ] = useState(() => {
         const cartItem = cart.find(cartItem=>{
@@ -37,6 +39,21 @@ export function CoffeeCard ({coffe}:CoffeeCardProps){
     
     console.log(cart)
    
+    const productRef = useRef<HTMLDivElement | null>(null);
+    
+    useEffect(() => {
+        if (productRef.current) {
+          ScrollReveal().reveal(productRef.current, {
+            origin: 'bottom',
+            delay: 500,
+            duration: 750,
+            distance: '40px',
+            easing: 'cubic-bezier(0.5, 0, 0, 1)',
+            // reset:true
+          });
+        }
+      }, []);
+
     const formatedPrice = coffe.price.toLocaleString('pt-BR', {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
@@ -46,7 +63,7 @@ export function CoffeeCard ({coffe}:CoffeeCardProps){
     
     
     return(
-        <CoffeeCardContainer>
+        <CoffeeCardContainer ref={productRef}>
             <img src={coffe.img} alt=""/>
             {/* <br /> */}
             <div style={{display: 'flex', gap: '4px'}}>
--- src/components/CoffeeList/index.tsx
+++ src/components/CoffeeList/index.tsx
@@ -1,16 +1,14 @@
-import { useContext } from "react";
 import { CoffeeCard } from "../CoffeCard";
 import { CoffeeListContainer } from "./styles";
-import { ShoppingCartContext } from "../../context/ShoppingCartContext";
 import { coffees } from "../../utils/CoffeList";
 
 export function CoffeeList (){
 
-    // const {} = useContext(ShoppingCartContext
+
     return(
-        <CoffeeListContainer>
+        <CoffeeListContainer >
             <h2>Nossos Cafés</h2>
-            <div>
+            <div  >
                 {
                     coffees.map(coffe=>{
                         return(
